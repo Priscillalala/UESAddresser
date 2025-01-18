52eff84d46aa1ef36b2b1e61c2f44600485034f4
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-01-17 22:38:46 -0800

    remove reorder list

--- Editor/ShippingAddresser.cs
+++ Editor/ShippingAddresser.cs
@@ -3,86 +3,16 @@ using SmartAddresser.Editor.Core.Models.LayoutRules;
 using SmartAddresser.Editor.Core.Models.LayoutRules.LabelRules;
 using SmartAddresser.Editor.Core.Models.Shared.AssetGroups;
 using SmartAddresser.Editor.Core.Models.Shared.AssetGroups.AssetFilterImpl;
-using SmartAddresser.Editor.Core.Tools.Addresser.Shared;
 using SmartAddresser.Editor.Core.Tools.Shared;
-using SmartAddresser.Editor.Foundation.TinyRx.ObservableCollection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using UESAddresser.Editor.AssetFilters;
 using UESAddresser.Editor.LabelProviders;
 using UnityEditor;
-using UnityEngine;
 
 namespace UESAddresser.Editor
 {
     public class ShippingAddresser
     {
-        public class ReorderLabelRules : IObserver<ListAddEvent<LabelRule>>, IDisposable
-        {
-            private static readonly Dictionary<IObservableList<LabelRule>, ReorderLabelRules> observers = new();
-
-            private IObservableList<LabelRule> labelRules;
-            private HashSet<LabelRule> seen;
-            private IDisposable addSubscription;
-
-            private ReorderLabelRules(IObservableList<LabelRule> labelRules)
-            {
-                this.labelRules = labelRules;
-                seen = new HashSet<LabelRule>(labelRules);
-                addSubscription = labelRules.ObservableAdd.Subscribe(this);
-            }
-
-            public void OnNext(ListAddEvent<LabelRule> listAddEvent)
-            {
-                if (labelRules == null || !seen.Add(listAddEvent.Value))
-                {
-                    return;
-                }
-                for (int i = 0; i < labelRules.Count; i++)
-                {
-                    if (labelRules[i].LabelProvider.Value is RoR2LabelProvider)
-                    {
-                        labelRules.RemoveAt(listAddEvent.Index);
-                        labelRules.Insert(i, listAddEvent.Value);
-                        break;
-                    }
-                }
-            }
-
-            public void OnError(Exception error) { }
-
-            public void OnCompleted() 
-            {
-                observers.Remove(labelRules);
-                Dispose();
-            }
-
-            public void Dispose()
-            {
-                labelRules = null;
-                seen = null;
-                addSubscription.Dispose();
-                addSubscription = null;
-            }
-
-            public static void Subscribe(IObservableList<LabelRule> labelRules)
-            {
-                if (!observers.ContainsKey(labelRules))
-                {
-                    observers.Add(labelRules, new ReorderLabelRules(labelRules));
-                }
-            }
-
-            public static void Unsubscribe(IObservableList<LabelRule> labelRules)
-            {
-                if (observers.Remove(labelRules, out ReorderLabelRules observer))
-                {
-                    observer.Dispose();
-                }
-            }
-        }
-
         const string MENU_ROOT = "Tools/UES Addresser/";
         const string DATA_CONTEXT_ROOT = "CONTEXT/" + nameof(LayoutRuleData) + "/";
 
@@ -173,34 +103,16 @@ namespace UESAddresser.Editor
 
                 layoutRule.LabelRules.Add(labelRule);
             }
-            ReorderLabelRules.Subscribe(layoutRule.LabelRules);
         }
 
         public static void RemoveRoR2LabelRules(LayoutRule layoutRule)
         {
-            ReorderLabelRules.Unsubscribe(layoutRule.LabelRules);
             for (int i = layoutRule.LabelRules.Count - 1; i >= 0; i--)
             {
                 LabelRule labelRule = layoutRule.LabelRules[i];
                 if (labelRule.LabelProvider.Value is RoR2LabelProvider)
                 {
                     layoutRule.LabelRules.RemoveAt(i);
-                }
-            }
-        }
-
-        [InitializeOnLoadMethod]
-        public static void InitializeOnLoad()
-        {
-            Debug.Log("InitializeOnLoad");
-            LayoutRuleDataRepository dataRepository = new LayoutRuleDataRepository();
-            foreach (var data in dataRepository.LoadAll())
-            {
-                Debug.Log("data");
-                if (data.LayoutRule != null && data.LayoutRule.LabelRules.Any(x => x.LabelProvider.Value is RoR2LabelProvider))
-                {
-                    Debug.Log("subbed");
-                    ReorderLabelRules.Subscribe(data.LayoutRule.LabelRules);
                 }
             }
         }
